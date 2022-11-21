5263e0f775d6747b5b941793c3541581c72fe74a
Tyler-Will <tylerceeme@gmail.com>
2022-11-20 20:36:01 -0800

    Added validation

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,10 +8,12 @@ function show (data) {
           <main>
             <h1>{data.place.name}</h1>
             <img id='showContent' src={data.place.pic}/>
-            <h1>Rating</h1>
+            <h1 id='showContent'>Rating</h1>
             <p id='showContent'>Not Rated</p>
             <h1>Description & Location</h1>
-            <p id='showContent'>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+            <h3 id='showContent'>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
+            <h1 id='showContent'>Comments</h1>
+            <p id='showContent'>No Comments</p>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>     
           <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
            <button type="submit" className="btn btn-danger">
